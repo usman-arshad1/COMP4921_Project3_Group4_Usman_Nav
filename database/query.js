cf7db6fcc9833feb90ff8b90783914fe0de91664
Usman Arshad <uarshad2012@gmail.com>
2023-11-16 17:39:36 -0800

    querys for login and signup

--- database/query.js
+++ database/query.js
@@ -1,2 +1,50 @@
 const database = include('dbConnection');
 
+async function createUser(postData) {
+	let createUserSQL = `
+		INSERT INTO user (username, email, hashed_pass, user_type_id) 
+		VALUES (:user, :email, :hashed_pass, 1);
+	`;
+	let params = {
+		user: postData.username,
+		email: postData.email,
+		hashed_pass: postData.hashed_pass
+	}
+	
+	try {
+		const results = await database.query(createUserSQL, params);
+
+		return true;
+	}
+	catch(err) {
+		console.log("Error inserting user");
+        console.log(err);
+		return false;
+	}
+}
+
+async function getUser(postData) {
+	let getUserSQL = `
+		SELECT *
+		FROM user
+		JOIN user_type USING(user_type_id)
+		WHERE username = :username;
+	`;
+
+	let params = {
+		username: postData.username
+	}
+	
+	try {
+		const results = await database.query(getUserSQL, params);
+
+		return results[0];
+	}
+	catch(err) {
+		console.log("Error trying to find user");
+        console.log(err);
+		return false;
+	}
+}
+
+module.exports = {getUser, createUser}
